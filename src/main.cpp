46f9d7ecb098b40bbe44a6e7a4e02b622cb857bb
VidsRox <vidyunagarwal@gmail.com>
2026-02-23 15:07:57 +0530

    Refactor file handling and threading in main process

--- src/main.cpp
+++ src/main.cpp
@@ -3,18 +3,23 @@
 #include <fstream>
 #include <queue>
 #include <mutex>
+#include <thread>
+#include <filesystem>
+
+using namespace std;
+namespace fs = std::filesystem; // alias to avoid typing std::filesystem every time
 
 class FileReader{//reads a file's contents
     private:
-        std::string file_path;
+        string file_path;
 
     public:
-        FileReader(std::string path) : file_path(path) {}//constructor
+        FileReader(string path) : file_path(path) {}//constructor
         
-        std::string read() {
-            std::ifstream file(file_path);//open the file first
+        string read() {
+            ifstream file(file_path);//open the file first
 
-            std::string contents((std::istreambuf_iterator<char>(file)), 
+            string contents((std::istreambuf_iterator<char>(file)), 
                                 std::istreambuf_iterator<char>());
 
             return contents;
@@ -23,13 +28,13 @@ class FileReader{//reads a file's contents
 
 class FileWriter{//write contents to a file
     private:
-    std::string file_path;
+    string file_path;
 
     public:
-        FileWriter(std::string path) : file_path(path) {}//constructor
+        FileWriter(string path) : file_path(path) {}//constructor
         
-        void write(std::string contents) {
-            std::ofstream file(file_path);//open file
+        void write(string contents) {
+            ofstream file(file_path);//open file
 
             file << contents;//write contents to it
         }
@@ -41,10 +46,10 @@ enum class Action {
 };
 
 struct Task{//describes a job (file path + action)
-    std::string file_path;
+    string file_path;
     Action action;
 
-    Task(std::string path, Action act): file_path(path), action(act) {}
+    Task(string path, Action act): file_path(path), action(act) {}
 };
 
 class Cryptor{//XOR transforms contents
@@ -54,7 +59,7 @@ class Cryptor{//XOR transforms contents
     public:
         Cryptor(char key) : encryption_key(key) {}
 
-        std::string transform(std::string contents) {/*because contents is passed by value (a copy), 
+        string transform(string contents) {/*because contents is passed by value (a copy), 
                                                     the copy is being modified and returned. 
                                                     The original string is untouched.*/
         for(int i = 0; i<contents.size(); i++){
@@ -67,11 +72,11 @@ class Cryptor{//XOR transforms contents
 
 char read_encryption_key(std::string env_file_path){
     
-    std::string line = FileReader(env_file_path).read();
+    string line = FileReader(env_file_path).read();
 
     int index = line.find('=');
 
-    std::string value = line.substr(index+1);
+    string value = line.substr(index+1);
     return value[0];
 }
 
@@ -79,6 +84,7 @@ class ProcessManager{
     private:
         std::queue<Task> t; 
         Cryptor cryptor;
+        mutex mtx;
 
     public:
         ProcessManager(char key): cryptor(key) {}
@@ -88,27 +94,40 @@ class ProcessManager{
         }
 
         void process() {
-            while(!t.empty()) {
+            while(true) {
+                unique_lock<mutex> lock(mtx);
+                if(t.empty()){
+                    break;
+                }
+                
                 Task task = t.front();//peek at front
                 t.pop();//remove it
+                lock.unlock();
                 
-                std::string contents = FileReader(task.file_path).read();//get the contents
+                string contents = FileReader(task.file_path).read();//get the contents
 
-                std::string transformed_contents = cryptor.transform(contents);
+                string transformed_contents = cryptor.transform(contents);
 
                 FileWriter(task.file_path).write(transformed_contents);
             }
         }
 };
 
-int main() {
+int main(int argc, char* argv[]) {
    char key = read_encryption_key(".env");
    
    ProcessManager pm(key);
 
-    Task t("test.txt", Action::ENCRYPT);
+   Action action = (string(argv[2]) == "ENCRYPT") ? Action::ENCRYPT : Action::DECRYPT;
 
-   pm.submit(t);
-   pm.process();
+   for (auto& entry : fs::directory_iterator(argv[1])){
+        pm.submit(Task(entry.path().string(), action));
+   }
+   
+   thread t1(&ProcessManager::process, &pm);
+   thread t2(&ProcessManager::process, &pm);
+
+   t1.join();
+   t2.join();
 
 }
